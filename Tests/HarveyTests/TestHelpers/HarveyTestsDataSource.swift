096641590ecf66370131a3a4a32d7795f6b8dbd7
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2017-10-23 21:17:34 +0200

    Add HarveyTestsDataSource

---
+++ Tests/HarveyTests/TestHelpers/HarveyTestsDataSource.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+final class HarveyTestsDataSource: HarveyDataSourceProtocol {
+
+    let shouldStubClosure: (URLRequest) -> Bool
+    let stubbedResponseClosure: (URLRequest) -> HarveyResponse?
+
+    init(shouldStubClosure: @escaping (URLRequest) -> Bool, stubbedResponseClosure: @escaping (URLRequest) -> HarveyResponse?) {
+        self.shouldStubClosure = shouldStubClosure
+        self.stubbedResponseClosure = stubbedResponseClosure
+    }
+
+    func shouldStub(for request: URLRequest) -> Bool {
+        return shouldStubClosure(request)
+    }
+
+    func stubbedResponse(for request: URLRequest) -> HarveyResponse? {
+        return stubbedResponseClosure(request)
+    }
+}
