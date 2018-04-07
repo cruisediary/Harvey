b88d5ea5e2f2a57ea1d102f5701028db8ab347f1
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2018-04-07 23:25:04 +0200

    Add FrameworkSpec for generating the project...

---
+++ Sources/Harvey.h
@@ -0,0 +1,14 @@
+//
+//  Harvey.h
+//  Harvey
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Harvey.
+FOUNDATION_EXPORT double HarveyVersionNumber;
+
+//! Project version string for Harvey.
+FOUNDATION_EXPORT const unsigned char HarveyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Harvey/PublicHeader.h>
